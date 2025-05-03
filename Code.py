1c2d4058b3de4ca878bb65cbe9cb4c7328656003
Golovanov Sergey <golovanov911@gmail.com>
2025-05-03 14:47:25 +0300

    Добавил точку входа в Code.py

--- Code.py
+++ Code.py
@@ -1,21 +1,27 @@
 # Напишите декоратор obfuscator
-def obfuscator(func):
-    def wrapper():
-        call_func = func()
-        make_list_name = list(call_func.get('name','Ключ отсутствует'))
-        make_list_password = len(list(call_func.get('password','Ключ отсутствует')))* '*'
-        for i in range(1, len(make_list_name)-1):
-            make_list_name[i] = '*'
-        call_func.update({"name": "".join(make_list_name), "password": make_list_password}) 
-        return call_func
-    return wrapper
+def main():
 
-@obfuscator
-def get_credentials():
-    return {
-        'name': 'StasBasov',
-        'password': 'iamthebest'
-    }
+    def obfuscator(func):
+        def wrapper():
+            call_func = func()
+            make_list_name = list(call_func.get('name','Ключ отсутствует'))
+            make_list_password = len(list(call_func.get('password','Ключ отсутствует')))* '*'
+            for i in range(1, len(make_list_name)-1):
+                make_list_name[i] = '*'
+            call_func.update({"name": "".join(make_list_name), "password": make_list_password}) 
+            return call_func
+        return wrapper
+
+    @obfuscator
+    def get_credentials():
+        return {
+            'name': 'StasBasov',
+            'password': 'iamthebest'
+        }
 
 
-print(get_credentials())
+    print(get_credentials())
+
+
+if __name__ == "__main__":
+    main()
